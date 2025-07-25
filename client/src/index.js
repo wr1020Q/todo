bc2a5df5385e66430630c519790f4a1acedef4b8
Hinan <hinan@example.com>
2025-07-25 14:43:19 +0900

    デプロイ修正

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,10 @@ import reportWebVitals from './reportWebVitals';
 import { worker } from './mocks/browser';
 
 async function main() {
+  if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
   // if (process.env.NODE_ENV === 'development') {
   //   await worker.start();
   // }
